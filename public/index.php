241e0068129b8b45b03944596f5fbefab91723d5
Zin Minn <zinminhtungit@gmail.com>
2022-01-10 15:32:58 +0630

    npm run build

--- public/index.php
+++ public/index.php
@@ -6,9 +6,9 @@
     <link rel="icon" href="<?= _public("favicon.ico");?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite App</title>
-      <script type="module" crossorigin src="<?= _public("/assets/index.fca9d7dc.js");?>"></script>
-      <link rel="modulepreload" href="<?= _public("/assets/vendor.91f6f067.js");?>">
-    <link rel="stylesheet" href="<?= _public("/assets/index.d1950162.css");?>">
+      <script type="module" crossorigin src="<?= _public("/assets/index.ed3b3972.js");?>"></script>
+      <link rel="modulepreload" href="<?= _public("/assets/vendor.b30c4864.js");?>">
+    <link rel="stylesheet" href="<?= _public("/assets/index.59bdd368.css");?>">
   </head>
   <body>
     <div id="app"></div>
